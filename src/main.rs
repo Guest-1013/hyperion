c0d89d748f9c3bd70cb4abeb4d3de410bd3d3153
Guest-1013 <guest_1013@foxmail.com>
2025-11-11 09:22:35 +0800

    打印离心率

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod hyperion;
 mod interface;
+use std::f64::consts::PI;
+
 use hyperion::*;
 use interface::*;
 
@@ -10,6 +12,7 @@ async fn main() {
     let mut game_state = GameState::Menu;
     let mut init_state = Hyperion::new(0.01, 0.0, 9.1, 1.35, 0.0, 0.5, 0.1);
     let mut states = HyperionStates{states: vec![init_state.clone()]};
+    let mut is_e_output = false;
     let mut is_saved = false;
     clear_background(BLACK);
     loop {
@@ -19,6 +22,11 @@ async fn main() {
                 states = HyperionStates{states: vec![init_state.clone()]};
             }
             GameState::Animation => {
+                if !is_e_output {
+                    let e = (1. - states.states.last().unwrap().vy.powf(2.) * states.states.last().unwrap().x / (4.*PI.powf(2.))).abs();
+                    println!("e = {}", e);
+                    is_e_output = true;
+                }
                 game_state.animation(&mut states).await;
             }
             
